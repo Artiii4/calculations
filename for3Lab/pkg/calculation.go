16e5d99ea1ec810b2756d194fab3c758e66c3588
Ефремов <efremov.ad@edu.spbstu.ru>
2023-11-04 21:36:29 +0300

    new

--- for3Lab/pkg/calculation.go
+++ for3Lab/pkg/calculation.go
@@ -6,25 +6,24 @@ import (
 
 func recursiveSearching(n int64) int64 {
 	var result int64
-	if n == 0 {
+	if n == 1 {
 		return result
 	} else {
-		if n > 0 {
-			result = n * recursiveSearching(n-1)
-		} else {
-			result = n * recursiveSearching(n+1)
-		}
+		result = n * recursiveSearching(n-1)
 	}
-	return -1
+	return result
 }
 func Calculate(n int64, check bool) int64 {
-	if n == 0 {
+	if n < 1 {
+		fmt.Println("ERROR! Factorial is considered for positive number")
 		return 0
 	}
 	if check == true {
 		fmt.Println("Start calculations...\nCalculate <", n, ">!")
 	}
 	result := recursiveSearching(n)
-	fmt.Println("Calculations complete!")
+	if check == true {
+		fmt.Println("Calculations complete!")
+	}
 	return result
 }
